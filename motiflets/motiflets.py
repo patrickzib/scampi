c94866118b4841fcfbd84b0c820f0f7d758cf562
Patrick Schäfer <patrick.schaefer@hu-berlin.de>
2026-03-22 22:18:31 +0100

    updates

--- motiflets/motiflets.py
+++ motiflets/motiflets.py
@@ -98,16 +98,13 @@ def convert_to_2d(
     return series
 
 
-def flatten_elbows(elbow_points, candidates, dists, max_items=None, k_max=None):
+def flatten_elbows(elbow_points, candidates, dists, max_items=None):
     if not isinstance(elbow_points, list):
         return elbow_points, candidates, dists
 
     items = []
     for rank in range(len(elbow_points)):
         elbows = elbow_points[rank]
-        if k_max is not None and rank == 0:
-            if k_max not in elbows:
-                elbows = np.append(elbows, np.int32(k_max))
         for k in elbows:
             if candidates[k] is None:
                 continue
