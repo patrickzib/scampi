03653474ed20f0bdf8c57beb738485ca8c486c4a
Patrick Schäfer <patrick.schaefer@hu-berlin.de>
2026-08-14 22:40:52 +0200

    cleanup

--- scampi/scampi.py
+++ scampi/scampi.py
@@ -566,7 +566,7 @@ def pd_series_to_numpy(data):
         data_raw = data
         data_index = np.arange(data.shape[-1])
     try:
-        return data_index.astype(np.float64), data_raw.astype(np.float64, copy=False
+        return data_index.astype(np.float64), data_raw.astype(np.float64, copy=False)
     except TypeError:  # datetime index cannot be cast to float64
         return data_index, data_raw.astype(np.float64, copy=False)
 
